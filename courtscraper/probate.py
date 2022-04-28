1c1fe76cd6fdf0e3bb8592b60bb970fd4c22f432
Fatima Gomez <fatima.gomez@datamade.us>
2022-04-28 15:05:25 -0500

    successful search response

--- courtscraper/probate.py
+++ courtscraper/probate.py
@@ -1,12 +1,28 @@
 import requests
+import lxml.html
 
 class ProbateScraper(requests.Session):
     def scrape(self):
         url = 'https://casesearch.cookcountyclerkofcourt.org/ProbateDocketSearch.aspx'
         response = requests.get(url).text
 
-        breakpoint()
+        tree = lxml.html.fromstring(response)
+        viewstate = tree.xpath("//input[@id='__VIEWSTATE']")[0].value
+        viewstategenerator = tree.xpath("//input[@id='__VIEWSTATEGENERATOR']")[0].value
+        eventvalidation = tree.xpath("//input[@id='__EVENTVALIDATION']")[0].value
 
+        # searching by case number is the default; no need to change form type
+        request_body = {
+            '__VIEWSTATE': viewstate,
+            '__VIEWSTATEGENERATOR': viewstategenerator,
+            '__EVENTVALIDATION': eventvalidation,
+            'ctl00$MainContent$rblSearchType': 'CaseNumber',
+            'ctl00$MainContent$txtCaseYear': '2021',
+            'ctl00$MainContent$txtCaseCode': 'P',
+            'ctl00$MainContent$txtCaseNumber': '000001',
+            'ctl00$MainContent$btnSearch': 'Start New Search'
+        }
+        search_response = requests.post(url, data=request_body).text
 
 scraper = ProbateScraper()
 scraper.scrape()
