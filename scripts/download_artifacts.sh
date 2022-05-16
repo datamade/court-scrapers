ea6a2273b809ca3cac9a3259d5a739ab7cfb3a62
msj <monkruman.st.jules@datamde.us>
2022-05-16 12:17:09 -0400

    Debug gitlab api curl

--- scripts/download_artifacts.sh
+++ scripts/download_artifacts.sh
@@ -9,7 +9,7 @@ fetch_artifacts() {
   curl --output artifacts.zip --header "PRIVATE-TOKEN: $CI_JOB_TOKEN" https://gitlab.com/api/v4/projects/36119029/jobs/$id/artifacts
 }
 
-curl --globoff --header 'JOB-TOKEN: $CI_JOB_TOKEN' https://gitlab.com/api/v4/projects/36119029/jobs | jq 'first(.[] | select(.name == "job1").id)'
+curl --globoff --header 'JOB-TOKEN: $CI_JOB_TOKEN' https://gitlab.com/api/v4/projects/36119029/jobs
 job_id=$(get_job_id)
 fetch_artifacts $job_id
 unzip -j artifacts.zip
