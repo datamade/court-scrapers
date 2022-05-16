2c6e5862644c5ade0e1403e0810c1675da91c2e1
msj <monkruman.st.jules@datamde.us>
2022-05-16 10:45:48 -0400

    Added script to download artifacts from upstream

---
+++ scripts/download_artifacts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+get_job_id() {
+  curl --globoff --header 'JOB-TOKEN: $CI_JOB_TOKEN' https://gitlab.com/api/v4/projects/36119029/jobs | jq 'first(.[] | select(.name == "job1").id)'
+}
+
+fetch_artifacts() {
+  id=$1
+  curl --output artifacts.zip --header "PRIVATE-TOKEN: $CI_JOB_TOKEN" https://gitlab.com/api/v4/projects/36119029/jobs/$id/artifacts
+}
+
+job_id=$(get_job_id)
+fetch_artifacts $job_id
+unzip -j artifacts.zip
+cat artifacts_1.txt
+cat artifacts_2.txt
+
