804c4830b674a978005e59e47878c65adf8d8945
Fatima Gomez <fatima.gomez@datamade.us>
2022-05-17 10:58:56 -0500

    final pseudocode

--- courtscraper/probate.py
+++ courtscraper/probate.py
@@ -73,51 +73,62 @@ class ProbateScraper(requests.Session):
             'case_type': case_type.strip()
         }
 
-    def get_parties_from_search_results(self, html_tree):
-        # case_data = [
-                # some_case_number: {
-                #     'case_number': some_case_number,
-                #     'filing_date': some_filing_date,
-                #     'claimants': [list_of_claimants]
-                # }
-            # ]
-            # next, get the table and iterate through the rows to find all rows referring to
-            # the same case number
-            # we can use this to add a "filing_date," which is often blank on the case detail page,
-            # in addition to adding all names in the `Claimant, Minor or Representative` column.
-            # If this case number is the last on a page (there are 10 rows per page of results),
-            # the above data obj will allow us to easily avoid duplicates of cases if a case
-            # has multiple rows that are broken up by a page of results.
+    def get_next_request_body(self, html_tree):
+        # use the html tree for current page of search results
+        # to return new request obj:
+        # next_request_body = {
+            # __EVENTTARGET: ctl00$MainContent$grdRecords
+            # __EVENTARGUMENT: Page$6
+            # __VIEWSTATE: [from current html_tree]
+            # __VIEWSTATEGENERATOR: [from current html_tree]
+            # __EVENTVALIDATION: [from current html_tree]
+        # }
 
-            # finally, also return the following info to get the next page of results:
-            # next_request_body = {
-                # __EVENTTARGET: ctl00$MainContent$grdRecords
-                # __EVENTARGUMENT: Page$6
-                # __VIEWSTATE: [from current html_tree]
-                # __VIEWSTATEGENERATOR: [from current html_tree]
-                # __EVENTVALIDATION: [from current html_tree]
-            # }
+        # if request fails due to page being out of range, return None
+        # else, return
+        pass
 
-            # return case_data, next_request_body
+    def iterate_search_results(self, date, current_page_number=1):
+        # first, send post request to url using:
+        # request_body = {
+            # '__VIEWSTATE': viewstate,
+            # '__VIEWSTATEGENERATOR': viewstategenerator,
+            # '__EVENTVALIDATION': eventvalidation,
+            # ctl00$MainContent$rblSearchType: FilingDate
+            # ctl00$MainContent$dtTxt: date (date as string, e.g. '8/3/2021')
+            # ctl00$MainContent$btnSearch: Start New Search
+        # }
+        # result_page = get(url, ...)
+
+        # case_data = {
+            # 'case_number': some_case_number,
+            # 'claimants': a_string
+        # }
+
+        # parse out rows from table in a loop && yield data using template ^^
+
+        # we can use this to add a "filing_date," which is often blank on the case detail page,
+        # in addition to adding all names in the `Claimant, Minor or Representative` column.
+        # If this case number is the last on a page (there are 10 rows per page of results),
+        # the above data obj will allow us to easily avoid duplicates of cases if a case
+        # has multiple rows that are broken up by a page of results.
+
+        # if current page number != last cell in fancy table in table pagination thing
+            # self.iterate_search_results(date, current_page_number + 1)
+            # [handle combination in get_search_results]
         pass
 
     def get_search_results(self, url, year='2021'):
         # for weekday in year:
-            # first, send post request to url using:
-            # request_body = {
-                # '__VIEWSTATE': viewstate,
-                # '__VIEWSTATEGENERATOR': viewstategenerator,
-                # '__EVENTVALIDATION': eventvalidation,
-                # ctl00$MainContent$rblSearchType: FilingDate
-                # ctl00$MainContent$dtTxt: 8/3/2021 (date as string)
-                # ctl00$MainContent$btnSearch: Start New Search
+            # case_data = {
+            #     'case_number': None,
+            #     'filing_date': date,
+            #     'claimants': [],
             # }
 
-            # case_data, next_request_body = self.get_parties_from_search_results(html_tree)
-
-            # TODO: how to address pagination?
-
-            # I would return list(case_data.values())
+            # for result in self.iterate_search_results(date):
+            #   check if file with this case # already exists; if so, update claimants
+            #   if not, yield case_data
         pass
 
     def scrape(self, url, year='2021', division_code='P', first_case_number=1, final_case_number=15000):
@@ -128,8 +139,7 @@ class ProbateScraper(requests.Session):
         # we will likely have to get new `viewstate`, `viewstategenerator`, and `eventvalidation` values
         # in order to search by case number
 
-        # loop over cases instead incrementally over all possible case numbers
-
+        # loop over cases instead of incrementally over all possible case numbers
         # searching by case number is the default; no need to change form type
         for i in range(first_case_number, final_case_number + 1):
             request_body = {
