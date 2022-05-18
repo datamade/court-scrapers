286fd0471edb6bbb4042625ccf9fad858097209e
msj <monkruman.st.jules@datamde.us>
2022-05-18 13:13:08 -0400

    Use download_fileobj to get s3 object

--- scripts/download_artifacts.py
+++ scripts/download_artifacts.py
@@ -4,8 +4,11 @@ import boto3
 def main():
     client = boto3.client('s3')
 
-    client.download_file('court-scrapers', 'artifact_1.txt', 'artifact_1.txt')
-    client.download_file('court-scrapers', 'artifact_2.txt', 'artifact_2.txt')
+    with open('artifact_1.txt', 'wb') as f:
+        client.download_fileobj('court-scrapers', 'artifact_1.txt', f)
+
+    with open('artifact_2.txt', 'wb') as f:
+        client.download_fileobj('court-scrapers', 'artifact_2.txt', f)
 
     with open('artifact_1.txt') as f:
         print(f.read())
