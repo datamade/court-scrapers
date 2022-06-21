68fe0bb0c1cdf527769ccf7ca9a68e8aa1c7c32d
Fatima Gomez <fatima.gomez@datamade.us>
2022-06-21 15:29:53 -0500

    upload sample files

--- scripts/upload_scrapes.py
+++ scripts/upload_scrapes.py
@@ -1,10 +1,19 @@
-import boto3
+import os
 import os
 from datetime import datetime
 
+import boto3
+from dotenv import dotenv_values
+
 
 def main():
-    client = boto3.client('s3')
+    key_id, access_key = dotenv_values(".env").values()
+    client = boto3.client('s3',
+        aws_access_key_id=key_id,
+        aws_secret_access_key=access_key
+    )
+
+    breakpoint()
 
     with open('data/artifact_1.txt', 'w') as f:
         f.write(f'Hello from... ({datetime.today().isoformat()})')
