67cc0df599441f816afa07e15ca400e9d3c40580
Fatima Gomez <fatima.gomez@datamade.us>
2022-06-21 16:02:28 -0500

    upload json files

--- scripts/upload_scrapes.py
+++ scripts/upload_scrapes.py
@@ -13,17 +13,12 @@ def main():
         aws_secret_access_key=access_key
     )
 
-    breakpoint()
+    scrapes_dir = 'courtscraper/scrape'
+    for file in os.listdir(scrapes_dir):
+        if file == '.gitkeep':
+            continue
 
-    with open('data/artifact_1.txt', 'w') as f:
-        f.write(f'Hello from... ({datetime.today().isoformat()})')
-
-    with open('data/artifact_2.txt', 'w') as f:
-        f.write(f'...an artifact! ({datetime.today().isoformat()})')
-
-    client.upload_file(os.path.abspath('data/artifact_1.txt'), 'court-scrapers', 'artifact_1.txt')
-    client.upload_file(os.path.abspath('data/artifact_2.txt'), 'court-scrapers', 'artifact_2.txt')
-
+        client.upload_file(os.path.abspath(f'{scrapes_dir}/{file}'), 'court-scrapers', f'{file}')
 
 if __name__ == '__main__':
     main()
