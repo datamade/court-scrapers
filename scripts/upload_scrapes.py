c5a70ab2c029ca0a3214598949342b0842e841f1
Fatima Gomez <fatima.gomez@datamade.us>
2022-06-27 15:27:25 -0500

    remove python-dotenv

--- scripts/upload_scrapes.py
+++ scripts/upload_scrapes.py
@@ -2,15 +2,10 @@ import os
 from datetime import datetime
 
 import boto3
-from dotenv import dotenv_values
 
 
 def main():
-    key_id, access_key = dotenv_values(".env").values()
-    client = boto3.client('s3',
-        aws_access_key_id=key_id,
-        aws_secret_access_key=access_key
-    )
+    client = boto3.client('s3')
 
     scrapes_dir = 'courtscraper/scrape'
     for file in os.listdir(scrapes_dir):
